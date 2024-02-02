5f6d15950cbcb78c3f86ebde489221f9189d8cf2
alihinnawe <hinnaweali@gmail.com>
2024-02-02 17:10:05 +0100

    add third challenge regarding chance

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
-
-// export const server = …
+import Chance from "chance";
+const chance = new Chance();
+const name1 = chance.name();
+const age = chance.age();
+const profession = chance.profession();
+console.log(name1, age, profession);
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.end(
+    `Hello, my name is ${name1} and I am ${age} years old. I am a ${profession}.`
+  );
+});
