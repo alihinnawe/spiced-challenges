46d9b5b3fbbb5868912a7cd8df05a026424dc0f0
alihinnawe <hinnaweali@gmail.com>
2024-02-07 15:01:05 +0100

    upload the update and delete backend challenge

---
+++ backend-update-and-delete/products/components/ProductForm/index.js
@@ -0,0 +1,44 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+// import useSWR from "swr";
+
+export default function ProductForm({ onSubmit }) {
+  // const products = useSWR("/api/products");
+
+  // async function handleSubmit(event) {
+  // event.preventDefault();
+  // console.log("Event: ", event);
+
+  // const formData = new FormData(event.target);
+  // const data = Object.fromEntries(formData);
+
+  //   onSubmit(event);
+  // }
+
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input type="number" id="price" name="price" min="0" />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
